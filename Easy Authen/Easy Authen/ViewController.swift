b9e0a86dad759463187b079a2fa29bbd1b7cffc2
paradoxSP <paradoxjava@gmail.com>
2017-12-10 14:15:28 +0700

    Frist

--- Easy Authen/Easy Authen/ViewController.swift
+++ Easy Authen/Easy Authen/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Explicit
+    var userString: String = ""
+    var passwordString: String = ""
+    var displayStrings = [
+        "Have Space Please Fill All Blank",
+        "User False",
+        "Password False",
+        "Welcome Member"
+    ]
+    var dicMemberStrings = [
+        "User1":"1",
+        "User2":"2",
+        "User3":"3",    
+    ]
     
     
     
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var displayTextField: UILabel!
     
     
+    @IBAction func loginButton(_ sender: Any) {
+        userString = userTextField.text!
+        passwordString = passwordTextField.text!
+        
+        // Show Log
+        print("User >> \(userString)")
+        print("Password >> \(passwordString)")
+        
+        // Check User
+        //print("Result >> \(dicMemberStrings[userString]!)")
+        
+        if let myCheckPassword = dicMemberStrings[userString] {
+            print("True Password >> \(myCheckPassword)")
+ 
+            if (passwordString == myCheckPassword) {
+                displayTextField.text = displayStrings[3]
+            }else{
+                displayTextField.text = displayStrings[2]
+            }
+        }else{
+            displayTextField.text = displayStrings[1]
+        }
+        
+    }
     
     
     override func viewDidLoad() {
---
+++ Funny Number/Funny Number/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Funny Number
+//
+//  Created by admin on 12/10/2560 BE.
+//  Copyright © 2560 paradoxjava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
